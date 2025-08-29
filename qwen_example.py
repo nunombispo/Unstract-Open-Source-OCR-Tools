7838a23f9733ecf961b9e5bc349da8f1ef030049
Nuno Bispo <nuno.m.bispo@gmail.com>
2025-08-29 14:56:08 +0200

    Add qwen_example.py for PDF processing and text extraction using the Qwen model; update requirements.txt to include ollama.

---
+++ qwen_example.py
@@ -0,0 +1,37 @@
+import sys
+from ollama import chat
+from ollama import ChatResponse
+import pypdfium2 as pdfium
+
+# Process the pdf file
+def process_pdf(pdf_path):
+    # Load a document
+    pdf = pdfium.PdfDocument(pdf_path)
+
+    # Loop over pages and render
+    image_list = []
+    for i in range(len(pdf)):
+        page = pdf[i]
+        image = page.render(scale=1).to_pil()
+
+        # Save image
+        file_name = f"docs/output/page_{i+1}.png"
+        image.save(file_name)
+        image_list.append(file_name)
+
+    print("Processing PDF file: ", pdf_path)
+    response: ChatResponse = chat(model='qwen2.5vl:7B', messages=[
+    {
+        'role': 'user',
+        'content': """You are a helpful assistant that can extract text from a pdf file passed in as individual images. 
+                        Please extract the text from the images and return it in a structured format.
+        """,
+        'images': image_list
+    },
+    ])
+    # Print the response
+    print(response.message.content)
+
+# Main function, receives the path to the pdf file
+if __name__ == "__main__":
+    process_pdf(sys.argv[1])
